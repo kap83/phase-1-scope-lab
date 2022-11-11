e0f79804fc21bc194fc092f5f5009cd1e898eeed
Karen Polanco <polanco.k@gmail.com>
2022-11-11 16:52:30 -0500

    commit tree 221380e54aa4a295c353ab62ce04608ed4f26f19

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+// did not work when i put var in front of bestCustomer b/c any it needs to be globally scoped. And variable defined without a keyword becomes globally scoped
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+//b/c we globally scoped bestCustomer in the function above, we were able to override it here
+
+const leastFavoriteCustomer = 'Seto is cute'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Seto is not cute'
+}
